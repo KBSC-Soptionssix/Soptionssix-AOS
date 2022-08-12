99667bc866a043b6985575246853aa9755aa8d94
KWY0218 <wodrk1001@gmail.com>
2022-08-12 16:30:45 +0900

    #1 buildSrc 모듈 추가
    dependency 들을 관리하는 모듈입니다
    추가 할 dependency가 있으면
    Versions , dependencies 파일 잘 확인해서 추가하면 됩니다

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,80 @@
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+object Dependencies {
+    // kotlin
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
+    const val coroutine =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutineVersion}"
+
+    // androidx
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayoutVersion}"
+    const val material = "com.google.android.material:material:${Versions.materialVersion}"
+
+    const val hiltClassPath = "com.google.dagger:hilt-android-gradle-plugin:${Versions.hiltVersion}"
+    const val hilt = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
+    const val hiltKapt = "com.google.dagger:hilt-android-compiler:${Versions.hiltVersion}"
+
+    const val coreKTX = "androidx.core:core-ktx:${Versions.coreKtxVersion}"
+    const val activityKTX = "androidx.activity:activity-ktx:${Versions.activityKTXVersion}"
+    const val lifecycleKTX =
+        "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleKTXVersion}"
+    const val fragmentKTX = "androidx.fragment:fragment-ktx:${Versions.fragmentKTXVersion}"
+
+    const val composeActivity =
+        "androidx.activity:activity-compose:${Versions.composeActivityVersion}"
+    const val composeAnimation = "androidx.compose.animation:animation:${Versions.composeVersion}"
+    const val composeUi = "androidx.compose.ui:ui-tooling:${Versions.composeVersion}"
+    const val composeViewModel =
+        "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.composeViewModelVersion}"
+    const val composeTheme =
+        "com.google.accompanist:accompanist-appcompat-theme:${Versions.composeThemeVersion}"
+    const val composeAdapter =
+        "com.google.android.material:compose-theme-adapter:${Versions.composeAdapterVersion}"
+    const val composeMaterial = "androidx.compose.material:material:${Versions.composeVersion}"
+
+    // test
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val androidTest = "androidx.test.ext:junit:${Versions.androidTestVersion}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+
+    // Third Party
+    const val coil = "io.coil-kt:coil:${Versions.coilVersion}"
+    const val interceptor =
+        "com.squareup.okhttp3:logging-interceptor:${Versions.interceptorVersion}"
+    const val gson = "com.google.code.gson:gson:${Versions.gsonVersion}"
+    const val retrofit2 = "com.squareup.retrofit2:retrofit:${Versions.retrofit2Version}"
+    const val retrofit2Converter =
+        "com.squareup.retrofit2:converter-gson:${Versions.retrofit2Version}"
+}
+
+fun DependencyHandler.hilt() {
+    implementation(Dependencies.hilt)
+    kapt(Dependencies.hiltKapt)
+}
+
+fun DependencyHandler.compose() {
+    implementation(Dependencies.composeUi)
+    implementation(Dependencies.composeActivity)
+    implementation(Dependencies.composeAnimation)
+    implementation(Dependencies.composeViewModel)
+    implementation(Dependencies.composeTheme)
+    implementation(Dependencies.composeAdapter)
+    implementation(Dependencies.composeMaterial)
+}
+
+fun DependencyHandler.network() {
+    implementation(Dependencies.gson)
+    implementation(Dependencies.retrofit2)
+    implementation(Dependencies.retrofit2Converter)
+    implementation(Dependencies.interceptor)
+}
+
+private fun DependencyHandler.implementation(depName: String) {
+    add("implementation", depName)
+}
+
+private fun DependencyHandler.kapt(depName: String) {
+    add("kapt", depName)
+}
