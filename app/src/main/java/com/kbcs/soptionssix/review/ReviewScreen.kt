fcc584230669ae4a6c30360f30242ad48f18d682
KWY0218 <wodrk1001@gmail.com>
2022-08-14 00:39:40 +0900

    #4 [add] screen 파일 추가

---
+++ app/src/main/java/com/kbcs/soptionssix/review/ReviewScreen.kt
@@ -0,0 +1,13 @@
+package com.kbcs.soptionssix.review
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ReviewScreen() {
+}
+
+@Preview
+@Composable
+fun ReviewScreenPreview() {
+}
