3487d7616b467a72dd3c6b003e82496df4c6ff96
KWY0218 <wodrk1001@gmail.com>
2022-08-14 01:47:57 +0900

    #4 [ui] 드롭 다운 메뉴 구현

---
+++ app/src/main/java/com/kbcs/soptionssix/review/component/DropDownMenu.kt
@@ -0,0 +1,53 @@
+package com.kbcs.soptionssix.review.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.DropdownMenu
+import androidx.compose.material.DropdownMenuItem
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.kbcs.soptionssix.R
+
+@Composable
+fun DropDownMenu(
+    menuItem: List<String>
+) {
+    var isExpanded by remember { mutableStateOf(false) }
+    Box() {
+        Image(
+            modifier = Modifier
+                .clickable { isExpanded = true },
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = ""
+        )
+
+        DropdownMenu(
+            modifier = Modifier,
+            expanded = isExpanded,
+            onDismissRequest = { isExpanded = false }
+        ) {
+            menuItem.forEach { menuText ->
+                DropdownMenuItem(
+                    onClick = { isExpanded = false }
+                ) {
+                    Text(text = menuText)
+                }
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DropDownMenuPreview() {
+    val previewList = mutableListOf("A", "B", "C", "D", "E")
+    DropDownMenu(previewList)
+}
