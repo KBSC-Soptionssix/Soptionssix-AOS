89645249a133cc4bc313a6a53010e9d7f8f00e6b
KWY0218 <wodrk1001@gmail.com>
2022-08-14 00:58:33 +0900

    #4 [ui] 기부하기/받기 공통 박스 구현

---
+++ app/src/main/java/com/kbcs/soptionssix/review/component/DonationBox.kt
@@ -0,0 +1,55 @@
+package com.kbcs.soptionssix.review.component
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kbcs.soptionssix.R
+
+@Composable
+fun DonationBox(
+    @StringRes contentText: Int,
+    @DrawableRes contentImage: Int
+) {
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(12.dp))
+    ) {
+        Row {
+            Text(
+                modifier = Modifier
+                    .padding(start = 16.dp, top = 16.dp),
+                text = stringResource(id = contentText)
+            )
+            Spacer(modifier = Modifier.size(22.dp))
+            Image(
+                modifier = Modifier
+                    .padding(end = 11.dp, top = 11.dp, bottom = 11.dp),
+                painter = painterResource(id = contentImage),
+                contentDescription = ""
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DonationBoxPreview() {
+    DonationBox(
+        contentText = R.string.app_name,
+        contentImage = R.drawable.ic_launcher_foreground
+    )
+}
