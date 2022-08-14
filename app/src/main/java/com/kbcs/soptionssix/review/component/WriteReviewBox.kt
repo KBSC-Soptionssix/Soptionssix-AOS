79280e6ef8eab70d58b4c178f38e75582bfda07f
KWY0218 <wodrk1001@gmail.com>
2022-08-14 17:59:55 +0900

    #4 [feat] 글쓰기 기능 구현

--- app/src/main/java/com/kbcs/soptionssix/review/component/WriteReviewBox.kt
+++ app/src/main/java/com/kbcs/soptionssix/review/component/WriteReviewBox.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,10 +15,15 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,7 +32,6 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,14 +51,13 @@ fun WriteReviewBox() {
 }
 
 @Composable
-private fun WriteReviewTextField(
-    reviewText: String = ""
-) {
+private fun WriteReviewTextField() {
+    var reviewText by remember { mutableStateOf("") }
     val maxChar = 300
     Column(modifier = Modifier.fillMaxWidth()) {
         BasicTextField(
-            value = reviewText.ifEmpty { stringResource(id = R.string.review_empty) },
-            onValueChange = {},
+            value = reviewText,
+            onValueChange = { if (it.length < maxChar) reviewText = it },
             textStyle = TextStyle(
                 color = colorResource(id = R.color.black)
             ),
@@ -63,20 +67,34 @@ private fun WriteReviewTextField(
                 .padding(16.dp)
                 .animateContentSize(),
             cursorBrush = SolidColor(colorResource(id = R.color.black))
-        )
+        ) { innerTextField ->
+            if (reviewText.isEmpty()) {
+                Text(
+                    text = stringResource(id = R.string.review_empty),
+                    color = Color.Gray
+                )
+            }
+            innerTextField()
+        }
         if (reviewText.isNotEmpty()) {
             Text(
                 modifier = Modifier.padding(start = 19.dp),
                 text = "${reviewText.length} / 300"
             )
             Spacer(modifier = Modifier.size(4.dp))
-            Text(
+            Box(
                 modifier = Modifier
-                    .padding(end = 16.dp, bottom = 8.dp)
-                    .fillMaxWidth(),
-                text = stringResource(id = R.string.registration),
-                textAlign = TextAlign.End
-            )
+                    .fillMaxWidth()
+                    .padding(end = 16.dp, bottom = 8.dp),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                Text(
+                    modifier = Modifier
+                        .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+                        .padding(vertical = 6.dp, horizontal = 14.dp),
+                    text = stringResource(id = R.string.registration)
+                )
+            }
         }
     }
 }
