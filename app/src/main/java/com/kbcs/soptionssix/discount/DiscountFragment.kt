113cc3e0a397360335e7b3785eedaa093c6d265e
hyeonjeong Park <py0429@ewhain.net>
2022-08-15 01:15:10 +0900

    #3 [ui] 반짝할인 뷰 생성

---
+++ app/src/main/java/com/kbcs/soptionssix/discount/DiscountFragment.kt
@@ -0,0 +1,20 @@
+package com.kbcs.soptionssix.discount
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kbcs.soptionssix.R
+
+class DiscountFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discount, container, false)
+    }
+
+}
