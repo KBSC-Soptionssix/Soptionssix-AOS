01813e4bf79829796c855df554180b158e7a2d2a
hyeonjeong Park <py0429@ewhain.net>
2022-08-16 18:14:01 +0900

    #3 [fix] 반짝할인 상품 item button -> text

--- app/src/main/java/com/kbcs/soptionssix/discount/DiscountFragment.kt
+++ app/src/main/java/com/kbcs/soptionssix/discount/DiscountFragment.kt
@@ -13,7 +13,7 @@ class DiscountFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_discount, container, false)
     }
 
---
+++ app/src/main/java/com/kbcs/soptionssix/discount/DiscountProductAllSeeActivity.kt
@@ -0,0 +1,11 @@
+package com.kbcs.soptionssix.discount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DiscountProductAllSeeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_discount_product_all_see)
+    }
+}
