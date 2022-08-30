28c38cdd61f62c362d836e266b7b6d7726498015
KWY0218 <wodrk1001@gmail.com>
2022-08-30 13:36:08 +0900

    #16 [ui] 버튼 내에 radius 적용

--- app/src/main/java/com/kbcs/soptionssix/util/component/ExchangeItem.kt
+++ app/src/main/java/com/kbcs/soptionssix/util/component/ExchangeItem.kt
@@ -9,12 +9,14 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -200,6 +202,7 @@ private fun ExchangeButton(
 ) {
     Box(
         modifier = modifier
+            .clip(RoundedCornerShape(4.dp))
             .background(backgroundColor)
             .clickable { clickEvent() }
     ) {
