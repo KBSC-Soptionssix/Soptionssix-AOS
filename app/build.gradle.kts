83fd676d0dc8b585b51190e4de2205e477d7b000
KWY0218 <wodrk1001@gmail.com>
2022-09-04 01:11:22 +0900

    #27 [feat] viewmodel과 screen 바인딩

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":data"))
     implementation("androidx.appcompat:appcompat:1.5.0")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
--- data/src/main/java/com/kbsc/data/dto/StorePreviewDto.kt
+++ data/src/main/java/com/kbsc/data/dto/StorePreviewDto.kt
@@ -1,17 +1,17 @@
 package com.kbsc.data.dto
 
 data class StorePreviewDto(
-    val id: String,
-    val photo: String?,
-    val name: String,
-    val category: String?,
-    val description: String?,
-    val maxDiscount: Int,
-    val discountStartTime: Int?,
-    val phone: String,
-    val breakStartTime: Int?,
-    val breakEndTime: Int?,
-    val startTime: Int,
-    val endTime: Int,
-    val hasChallenge: Boolean
+    val id: String = "",
+    val photo: String? = null,
+    val name: String = "",
+    val category: String? = null,
+    val description: String? = null,
+    val maxDiscount: Int = 0,
+    val discountStartTime: Int? = null,
+    val phone: String = "",
+    val breakStartTime: Int? = null,
+    val breakEndTime: Int? = null,
+    val startTime: Int = 0,
+    val endTime: Int = 0,
+    val hasChallenge: Boolean = true
 )
