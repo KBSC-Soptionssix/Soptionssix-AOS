db1589836dbab86d06423d997a4005d85fa15443
hyeonjeong Park <py0429@ewhain.net>
2022-09-05 18:09:48 +0900

    #37 [feat] 버튼 클릭시 교환완료로

--- app/src/main/java/com/kbcs/soptionssix/exchange/ExchangeDetailActivity.kt
+++ app/src/main/java/com/kbcs/soptionssix/exchange/ExchangeDetailActivity.kt
@@ -25,6 +25,7 @@ class ExchangeDetailActivity : AppCompatActivity() {
         }
 
         formatCost(9000)
+        completeExchange()
     }
 
     private fun formatCost(costs: Int) {
@@ -32,5 +33,16 @@ class ExchangeDetailActivity : AppCompatActivity() {
 
         binding.tvDiscountPrice.text = costFormatter.format(costs)
         binding.tvTotalPrice.text = costFormatter.format(costs)
+    }
+
+    private fun completeExchange() {
+        binding.ivCompleteExchange.setOnClickListener {
+            binding.ivCompleteExchange.background =
+                getDrawable(R.drawable.shape_gray_fills_4_rectangle)
+
+            binding.tvExchangeComplete.text = "교환"
+            binding.tvRemainedTime.text = " "
+            binding.tvRemained.text = "완료"
+        }
     }
 }
