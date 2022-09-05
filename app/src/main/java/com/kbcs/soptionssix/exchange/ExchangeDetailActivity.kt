18fbe8ebf996edf8329a8d18a9c1be550ae7174d
hyeonjeong Park <py0429@ewhain.net>
2022-09-05 23:44:45 +0900

    #37 [feat] 클립보드에 복사, 붙여넣기기능

--- app/src/main/java/com/kbcs/soptionssix/exchange/ExchangeDetailActivity.kt
+++ app/src/main/java/com/kbcs/soptionssix/exchange/ExchangeDetailActivity.kt
@@ -1,6 +1,11 @@
 package com.kbcs.soptionssix.exchange
 
+import android.content.ClipData
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -13,12 +18,14 @@ import java.text.DecimalFormat
 
 class ExchangeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExchangeDetailBinding
-    private val exchangeDetailViewModel by viewModels<ExchangeDetailViewModel>()
+    private val exchangeDetailViewModel: ExchangeDetailViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_exchange_detail)
+        binding.exchangeViewModel = exchangeDetailViewModel
+
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             replace<NaverMapFragment>(R.id.fcv_naver_map)
@@ -26,6 +33,8 @@ class ExchangeDetailActivity : AppCompatActivity() {
 
         formatCost(9000)
         completeExchange()
+        copyRoadClipData()
+        copyAddressClipData()
     }
 
     private fun formatCost(costs: Int) {
@@ -43,6 +52,36 @@ class ExchangeDetailActivity : AppCompatActivity() {
             binding.tvExchangeComplete.text = "교환"
             binding.tvRemainedTime.text = " "
             binding.tvRemained.text = "완료"
+        }
+    }
+
+    private fun copyRoadClipData() {
+        binding.tvCopyRoad.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("copyData", binding.tvRoadDetail.text)
+            // 클립보드에 배치
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "복사되었습니다.", Toast.LENGTH_SHORT).show()
+            checkPaste(clipboard)
+        }
+    }
+
+    private fun copyAddressClipData() {
+        binding.tvCopyAddress.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip: ClipData = ClipData.newPlainText("copyData", binding.tvAddressDetail.text)
+            // 클립보드에 배치
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "복사되었습니다.", Toast.LENGTH_SHORT).show()
+            checkPaste(clipboard)
+        }
+    }
+
+    private fun checkPaste(clipManager: ClipboardManager) {
+        when {
+            !clipManager.hasPrimaryClip() -> false
+            !(clipManager.primaryClipDescription?.hasMimeType(MIMETYPE_TEXT_PLAIN))!! -> false
+            else -> true
         }
     }
 }
---
+++ app/src/main/java/com/kbcs/soptionssix/exchange/ExchangeDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.kbcs.soptionssix.exchange
+
+import androidx.lifecycle.ViewModel
+
+class ExchangeDetailViewModel : ViewModel() {
+    val nick = 10
+}
