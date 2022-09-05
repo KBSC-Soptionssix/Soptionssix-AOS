70882868f613ca4e700d6f985d423b88f3e3b016
hyeonjeong Park <py0429@ewhain.net>
2022-09-05 17:51:36 +0900

    #37 [feat] 가격 컴마 설정

--- app/src/main/java/com/kbcs/soptionssix/exchange/ExchangeDetailActivity.kt
+++ app/src/main/java/com/kbcs/soptionssix/exchange/ExchangeDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.kbcs.soptionssix.exchange
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
@@ -8,15 +9,28 @@ import androidx.fragment.app.replace
 import com.kbcs.soptionssix.R
 import com.kbcs.soptionssix.databinding.ActivityExchangeDetailBinding
 import com.kbcs.soptionssix.navermap.NaverMapFragment
+import java.text.DecimalFormat
 
 class ExchangeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExchangeDetailBinding
+    private val exchangeDetailViewModel by viewModels<ExchangeDetailViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_exchange_detail)
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             replace<NaverMapFragment>(R.id.fcv_naver_map)
         }
+
+        formatCost(9000)
+    }
+
+    private fun formatCost(costs: Int) {
+        val costFormatter = DecimalFormat("###,###")
+
+        binding.tvDiscountPrice.text = costFormatter.format(costs)
+        binding.tvTotalPrice.text = costFormatter.format(costs)
     }
 }
