1cad473765ebb7686395b4dc66e0927da287e8da
hyeonjeong Park <py0429@ewhain.net>
2022-09-13 23:15:25 +0900

    #60 [feat] 반짝할인뷰 API 연결

---
+++ app/src/main/java/com/kbcs/soptionssix/discount/DiscountViewModel.kt
@@ -0,0 +1,45 @@
+package com.kbcs.soptionssix.discount
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.kbcs.data.repository.DiscountRepository
+import com.kbsc.data.dto.ProductDto
+import com.kbsc.data.dto.StoreDto
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DiscountViewModel @Inject constructor(
+    private val discountRepository: DiscountRepository
+) : ViewModel() {
+    private var _storeList = MutableLiveData<List<StoreDto>>()
+    val storeList: LiveData<List<StoreDto>> get() = _storeList
+
+    private var _productList = MutableLiveData<List<ProductDto>>()
+    val productList: LiveData<List<ProductDto>> get() = _productList
+
+    private var _waitingList = MutableLiveData<List<ProductDto>>()
+    val waitingList: LiveData<List<ProductDto>> get() = _waitingList
+
+    init {
+        viewModelScope.launch {
+            fetchDiscountList()
+        }
+    }
+
+    suspend fun fetchDiscountList() {
+        discountRepository.getDiscountList()
+            .onSuccess { DiscountDto->
+                _storeList.value = DiscountDto.storeList
+                _productList.value = DiscountDto.productList
+                _waitingList.value = DiscountDto.waitDonationList
+            }
+            .onFailure {
+                Log.d("DiscountViewModel", "error: ${it.message}")
+            }
+    }
+}
--- data/src/main/java/com/kbcs/data/repository/DiscountRepositoryImpl.kt
+++ data/src/main/java/com/kbcs/data/repository/DiscountRepositoryImpl.kt
@@ -1,5 +1,13 @@
 package com.kbcs.data.repository
 
+import com.kbcs.data.service.DiscountService
+import com.kbsc.data.dto.DiscountDto
 import javax.inject.Inject
 
-class DiscountRepositoryImpl @Inject constructor() : DiscountRepository
+class DiscountRepositoryImpl @Inject constructor(
+    private val discountService: DiscountService
+) : DiscountRepository {
+    override suspend fun getDiscountList(): Result<DiscountDto> {
+        return runCatching { discountService.getDiscountList() }
+    }
+}
